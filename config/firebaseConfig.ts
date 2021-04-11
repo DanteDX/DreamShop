ba38ebb56edb5d202465fb0592445fb0a45e40c9
YamatoDX <piyal@twurs.com>
2021-04-11 10:38:46 +0600

    firebaseconfig added

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDewia9chlllK_0EZEc9AdSNj91oZ6yrUs",
+  authDomain: "dreamshop-18f1d.firebaseapp.com",
+  projectId: "dreamshop-18f1d",
+  storageBucket: "dreamshop-18f1d.appspot.com",
+  messagingSenderId: "728284691286",
+  appId: "1:728284691286:web:e80844f60a7b4a0a0f74f7",
+  measurementId: "G-0VG81C17LX"
+};
